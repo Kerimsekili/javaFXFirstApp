39122b983de7ef0b461dbc69a5351b53ec65618b
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-08-22 19:33:31 +0300

    Adding Exit method

--- src/main/java/com/jmc/javafxfirstapp/HelloApplication.java
+++ src/main/java/com/jmc/javafxfirstapp/HelloApplication.java
@@ -7,6 +7,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,21 +18,23 @@ public class HelloApplication extends Application {
     public void start(Stage stage) {
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
-        root.setPadding(new Insets(20,20,20,20));
+        root.setPadding(new Insets(20, 20, 20, 20));
         root.setSpacing(20);
-        Label text = new Label("");
+        Label text = new Label("This is Label");
         Button button = new Button("Click Me");
         Button button1 = new Button("Exit Explicitly");
-        button.setOnAction(e->text.setText("Welcome to JavaFX Application !"));
-        button1.setOnAction(event -> Platform.exit());
-        root.getChildren().addAll(text,button,button1);
-        Scene scene = new Scene(root,350,300);
+        button.setOnAction(e -> text.setText("Welcome to JavaFX Application !"));
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> text.setText("Thank you for clicking the button !!"));
+        root.getChildren().addAll(text, button, button1);
+        Scene scene = new Scene(root, 350, 300);
         stage.setScene(scene);
+        stage.setOnShowing(windowEvent -> System.out.println("Stage is showing"));
         stage.setTitle("JavaFX Application");
         stage.show();
     }
+
     @Override
-    public void stop(){
+    public void stop() {
         System.out.println(Thread.currentThread().getName());
         System.out.println("Closing the Application");
     }
